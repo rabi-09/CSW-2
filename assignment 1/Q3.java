a6ffc4b744a45c8294832ba243c48b72dd4f40bf
Rabi Narayan Das <dasrabi2004@gmail.com>
2025-04-29 20:13:18 +0530

    Update Q3.java

--- assignment 1/Q3.java
+++ assignment 1/Q3.java
@@ -5,6 +5,10 @@ class Point{
         this.x = x;
         this.y = y;
     }
+    public Point(Point op){
+        this.x = op.x;
+        this.y = op.y;
+    }
 
     public double getX() {
         return x;
@@ -30,7 +34,7 @@ class Point{
 public class Q3 {
     public static void main(String[] args) {
         Point p1 = new Point(2,5);
-        Point p2 = new Point(2,5);
+        Point p2 = new Point(p1);
         p2.setX(3.5);
         p2.setY(6.5);
         System.out.println(p1);
