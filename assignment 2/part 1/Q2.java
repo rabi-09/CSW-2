4e67c8d1985696aa63e425d5bda1e9a39c6fd627
Rabi Narayan Das <dasrabi2004@gmail.com>
2025-04-29 21:52:03 +0530

    Update Q2.java

--- assignment 2/part 1/Q2.java
+++ assignment 2/part 1/Q2.java
@@ -25,24 +25,16 @@ class Book implements Comparable<Book>{
 public class Q2 {
     public static void main(String[] args) {
         Book b1 = new Book(1, "CSW", 499);
-        Book b2 = new Book(2, "AAd", 560);
+        Book b2 = new Book(2, "AAD", 560);
         Book b3 = new Book(1, "CSW", 499);
-        if(b1.compareTo(b2)==1){
+        if(b1.compareTo(b2)==0)
+            System.out.println("Both have Same Price");
+        else if(b1.compareTo(b2)>0){
             System.out.println(b1.bookName+" has More price");
         }
         else{
             System.out.println(b2.bookName+" has More price");
         }
         System.out.println("Is Book 1 and 2 same: "+ b1.equals(b3));
-        
-        // if(compare(b1,b2)==1){
-        //     System.out.println(b1.bookName+" has More price");
-        // }
-        // else{
-        //     System.out.println(b2.bookName+" has More price");
-        // }
     }
-    // public static int compare(Book b1, Book b2){
-    //     return Integer.compare(b1.price, b2.price);
-    // }
 }
